29438e6f87e1de7777694d4280b1dcf8aa5842f8
Aeron256 <aeronlumawag@gmail.com>
2026-04-24 14:30:50 +0800

    feat: setup react-router skeleton and ProtectedRoute guard

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,6 @@
+export default function Admin () {
+    return  (<div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
+        <p>Welcome to the admin dashboard. Use the navigation to manage your products, sales, and customers.</p>
+    </div>);
+}
